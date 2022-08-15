7365cb695df4b5c1c2c559f1050fe0a50384bc36
ziyacan yıldız <ziyacan.11@hotmail.com>
2022-08-15 18:44:55 +0300

    Add project files.

---
+++ PersonelForm/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿
+namespace PersonelForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMesaj = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGetir = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.txtTelNo = new System.Windows.Forms.TextBox();
+            this.txtMeslek = new System.Windows.Forms.TextBox();
+            this.txtSehir = new System.Windows.Forms.TextBox();
+            this.txtMaas = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.lblTelNo = new System.Windows.Forms.Label();
+            this.lblMeslek = new System.Windows.Forms.Label();
+            this.lblSehir = new System.Windows.Forms.Label();
+            this.lblMaas = new System.Windows.Forms.Label();
+            this.lblSoyad = new System.Windows.Forms.Label();
+            this.lblAd = new System.Windows.Forms.Label();
+            this.lblId = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblMesaj
+            // 
+            this.lblMesaj.BackColor = System.Drawing.SystemColors.Info;
+            this.lblMesaj.Enabled = false;
+            this.lblMesaj.Location = new System.Drawing.Point(526, 347);
+            this.lblMesaj.Multiline = true;
+            this.lblMesaj.Name = "lblMesaj";
+            this.lblMesaj.Size = new System.Drawing.Size(355, 30);
+            this.lblMesaj.TabIndex = 56;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(357, 29);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(602, 274);
+            this.dataGridView1.TabIndex = 55;
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.Location = new System.Drawing.Point(390, 347);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(85, 38);
+            this.btnTemizle.TabIndex = 54;
+            this.btnTemizle.Text = "Temizle";
+            this.btnTemizle.UseVisualStyleBackColor = true;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(299, 347);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(85, 38);
+            this.btnSil.TabIndex = 53;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGetir
+            // 
+            this.btnGetir.Location = new System.Drawing.Point(208, 347);
+            this.btnGetir.Name = "btnGetir";
+            this.btnGetir.Size = new System.Drawing.Size(85, 38);
+            this.btnGetir.TabIndex = 52;
+            this.btnGetir.Text = "Getir";
+            this.btnGetir.UseVisualStyleBackColor = true;
+            this.btnGetir.Click += new System.EventHandler(this.btnGetir_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(117, 347);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(85, 38);
+            this.btnGuncelle.TabIndex = 51;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(26, 347);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(85, 38);
+            this.btnKaydet.TabIndex = 50;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // txtTelNo
+            // 
+            this.txtTelNo.Location = new System.Drawing.Point(132, 300);
+            this.txtTelNo.Name = "txtTelNo";
+            this.txtTelNo.Size = new System.Drawing.Size(188, 22);
+            this.txtTelNo.TabIndex = 49;
+            // 
+            // txtMeslek
+            // 
+            this.txtMeslek.Location = new System.Drawing.Point(132, 254);
+            this.txtMeslek.Name = "txtMeslek";
+            this.txtMeslek.Size = new System.Drawing.Size(188, 22);
+            this.txtMeslek.TabIndex = 48;
+            // 
+            // txtSehir
+            // 
+            this.txtSehir.Location = new System.Drawing.Point(132, 204);
+            this.txtSehir.Name = "txtSehir";
+            this.txtSehir.Size = new System.Drawing.Size(188, 22);
+            this.txtSehir.TabIndex = 47;
+            // 
+            // txtMaas
+            // 
+            this.txtMaas.Location = new System.Drawing.Point(132, 159);
+            this.txtMaas.Name = "txtMaas";
+            this.txtMaas.Size = new System.Drawing.Size(188, 22);
+            this.txtMaas.TabIndex = 46;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(132, 115);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(188, 22);
+            this.txtSoyad.TabIndex = 45;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(132, 70);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(188, 22);
+            this.txtAd.TabIndex = 44;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(132, 29);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(188, 22);
+            this.txtId.TabIndex = 43;
+            // 
+            // lblTelNo
+            // 
+            this.lblTelNo.AutoSize = true;
+            this.lblTelNo.Location = new System.Drawing.Point(59, 300);
+            this.lblTelNo.Name = "lblTelNo";
+            this.lblTelNo.Size = new System.Drawing.Size(46, 17);
+            this.lblTelNo.TabIndex = 42;
+            this.lblTelNo.Text = "TelNo";
+            // 
+            // lblMeslek
+            // 
+            this.lblMeslek.AutoSize = true;
+            this.lblMeslek.Location = new System.Drawing.Point(59, 254);
+            this.lblMeslek.Name = "lblMeslek";
+            this.lblMeslek.Size = new System.Drawing.Size(52, 17);
+            this.lblMeslek.TabIndex = 41;
+            this.lblMeslek.Text = "Meslek";
+            // 
+            // lblSehir
+            // 
+            this.lblSehir.AutoSize = true;
+            this.lblSehir.Location = new System.Drawing.Point(59, 204);
+            this.lblSehir.Name = "lblSehir";
+            this.lblSehir.Size = new System.Drawing.Size(41, 17);
+            this.lblSehir.TabIndex = 40;
+            this.lblSehir.Text = "Sehir";
+            // 
+            // lblMaas
+            // 
+            this.lblMaas.AutoSize = true;
+            this.lblMaas.Location = new System.Drawing.Point(59, 159);
+            this.lblMaas.Name = "lblMaas";
+            this.lblMaas.Size = new System.Drawing.Size(42, 17);
+            this.lblMaas.TabIndex = 39;
+            this.lblMaas.Text = "Maaş";
+            // 
+            // lblSoyad
+            // 
+            this.lblSoyad.AutoSize = true;
+            this.lblSoyad.Location = new System.Drawing.Point(59, 115);
+            this.lblSoyad.Name = "lblSoyad";
+            this.lblSoyad.Size = new System.Drawing.Size(48, 17);
+            this.lblSoyad.TabIndex = 38;
+            this.lblSoyad.Text = "Soyad";
+            // 
+            // lblAd
+            // 
+            this.lblAd.AutoSize = true;
+            this.lblAd.Location = new System.Drawing.Point(59, 70);
+            this.lblAd.Name = "lblAd";
+            this.lblAd.Size = new System.Drawing.Size(25, 17);
+            this.lblAd.TabIndex = 37;
+            this.lblAd.Text = "Ad";
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(59, 29);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(21, 17);
+            this.lblId.TabIndex = 36;
+            this.lblId.Text = "ID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(989, 406);
+            this.Controls.Add(this.lblMesaj);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGetir);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.txtTelNo);
+            this.Controls.Add(this.txtMeslek);
+            this.Controls.Add(this.txtSehir);
+            this.Controls.Add(this.txtMaas);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lblTelNo);
+            this.Controls.Add(this.lblMeslek);
+            this.Controls.Add(this.lblSehir);
+            this.Controls.Add(this.lblMaas);
+            this.Controls.Add(this.lblSoyad);
+            this.Controls.Add(this.lblAd);
+            this.Controls.Add(this.lblId);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox lblMesaj;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGetir;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.TextBox txtTelNo;
+        private System.Windows.Forms.TextBox txtMeslek;
+        private System.Windows.Forms.TextBox txtSehir;
+        private System.Windows.Forms.TextBox txtMaas;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label lblTelNo;
+        private System.Windows.Forms.Label lblMeslek;
+        private System.Windows.Forms.Label lblSehir;
+        private System.Windows.Forms.Label lblMaas;
+        private System.Windows.Forms.Label lblSoyad;
+        private System.Windows.Forms.Label lblAd;
+        private System.Windows.Forms.Label lblId;
+    }
+}
+
---
+++ PersonelForm/Form1.cs
@@ -0,0 +1,152 @@
+﻿using PersonelManagerLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonelForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void temizle()
+        {
+            txtId.Text = "";
+            txtAd.Text = "";
+            txtSoyad.Text = "";
+            txtMaas.Text = "";
+            txtSehir.Text = "";
+            txtMeslek.Text = "";
+            txtTelNo.Text = "";
+
+        }
+        private void kayitGetir()  //Listele
+        {
+            PersonelManager personelManager = new PersonelManager();
+            dataGridView1.DataSource = personelManager.GetirHepsi();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            kayitGetir();
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            Personel personel = new Personel();
+            personel.Ad = txtAd.Text;
+            personel.Soyad = txtSoyad.Text;
+            personel.Maas = txtMaas.Text;
+            personel.Sehir = txtSehir.Text;
+            personel.Meslek = txtMeslek.Text;
+            personel.TelNo = txtTelNo.Text;
+
+            PersonelManager personelManager = new PersonelManager();
+
+            if (txtId.Text == "")
+            {
+                personelManager.Ekle(personel);
+                lblMesaj.Text = "Personel eklendi";
+                kayitGetir();
+                temizle();
+            }
+            else
+            {
+                lblMesaj.Text = "Personel eklenemedi , id alanını boş bırakın";
+                temizle();
+            }
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            Personel personel = new Personel();
+            personel.Id = Convert.ToInt32(txtId.Text);
+            personel.Ad = txtAd.Text;
+            personel.Soyad = txtSoyad.Text;
+            personel.Maas = txtMaas.Text;
+            personel.Sehir = txtSehir.Text;
+            personel.Meslek = txtMeslek.Text;
+            personel.TelNo = txtTelNo.Text;
+
+            PersonelManager personelManager = new PersonelManager();
+
+            if (personelManager.Guncelle(personel))
+            {
+                lblMesaj.Text = "Personel güncellendi";
+                kayitGetir();
+                temizle();
+            }
+            else
+            {
+                lblMesaj.Text = "Personel güncellenemedi , tekrar deneyin";
+                temizle();
+            }
+        }
+
+        private void btnGetir_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(txtId.Text);
+
+            PersonelManager personelManager = new PersonelManager();
+            Personel personel = personelManager.GetirById(id);
+
+            if (personel.Ad != null)
+            {
+                txtAd.Text = personel.Ad;
+                txtSoyad.Text = personel.Soyad;
+                txtMaas.Text = personel.Maas;
+                txtSehir.Text = personel.Sehir;
+                txtMeslek.Text = personel.Meslek;
+                txtTelNo.Text = personel.TelNo;
+
+                lblMesaj.Text = "Kayıt getirildi";
+            }
+            else
+            {
+                lblMesaj.Text = "Kayıt bulunamadı , geçerli bir id giriniz.";
+            }
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            PersonelManager personelManager = new PersonelManager();
+
+            if (personelManager.Sil(Convert.ToInt32(txtId.Text)))
+            {
+                lblMesaj.Text = "Personel silindi";
+                kayitGetir();
+                temizle();
+            }
+            else
+            {
+                lblMesaj.Text = "Personel silinemedi , tekrar deneyin";
+                temizle();
+            }
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            temizle();
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            int secim = dataGridView1.SelectedCells[0].RowIndex;
+            txtId.Text = dataGridView1.Rows[secim].Cells[0].Value.ToString();
+            txtAd.Text = dataGridView1.Rows[secim].Cells[1].Value.ToString();
+            txtSoyad.Text = dataGridView1.Rows[secim].Cells[2].Value.ToString();
+            txtMaas.Text = dataGridView1.Rows[secim].Cells[3].Value.ToString();
+            txtSehir.Text = dataGridView1.Rows[secim].Cells[4].Value.ToString();
+            txtMeslek.Text = dataGridView1.Rows[secim].Cells[5].Value.ToString();
+            txtTelNo.Text = dataGridView1.Rows[secim].Cells[6].Value.ToString();
+        }
+    }
+}
